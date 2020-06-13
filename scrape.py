baadb3767080630b8dc6d9b45176aa1903c442b2
IsaacAudet <isaac.audet@gmail.com>
2020-06-13 10:14:16 -0700

    naive search result pulls

--- scrape.py
+++ scrape.py
@@ -78,8 +78,7 @@ class Scrape:
     def bestbuy(self, sn=None):
 
         if sn is None:
-            self.get_content(
-                'https://www.bestbuy.ca/en-ca/product/amazon-fire-tv-stick-media-streamer-with-alexa-voice-remote/13365349')
+            self.get_content(self.url)
             soup = self.soup
             name = soup.find('h1', attrs={'class': 'productName_19xJx'})
             price = soup.find(
@@ -110,166 +109,244 @@ class Scrape:
             url = 'https://www.bestbuy.ca/en-ca/search?search=' + sn
             driver = webdriver.Firefox(options=options)
             driver.get(url)
+            try:
+                price = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, "//div[3]/span/div"))).text
+                print(price)
+            finally:
+                driver.quit()
+            return price
 
-            price = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//div[3]/span/div"))).text
-            print(price)
-            driver.quit()
+    def newegg(self, sn=None):
+        if sn is None:
+            # requests-html needed to parse JavaScript (price)
+            session = HTMLSession()
+            r = session.get(self.url)
+            r.html.render()
+
+            soup = self.soup
+
+            name = soup.find('span', {'style': 'display: inline;'})
+            price = r.html.find('li.price-current', first=True).text.strip()
+            # rating = soup.find('span', attrs={'class': 'print'})
+            asin = soup.find('dt', string='Model').next_sibling
+            date = today.strftime("%d/%m/%Y")
+            all = {}
+
+            if asin is not None:
+                all['id'] = asin.text
+            else:
+                all['id'] = '-1'
+
+            if name is not None:
+                all['name'] = name.text.strip()
+            else:
+                all['name'] = None
+
+            if price is not None:
+                all['price'] = {date: price.replace(
+                    u'\xa0', u'').replace(' –', '')}
+            else:
+                all['price'] = {date: 'CDN$0'}
+
+            return all
+
+        else:
+            # scrape search results using HTMLSession
+            url = 'https://www.newegg.ca/p/pl?d=' + sn
+            session = HTMLSession()
+            r = session.get(url)
+            r.html.render()
+            try:
+                price = r.html.find('li.price-current',
+                                    first=True).text.strip().replace(' –', '')
+                print(price)
+            except Exception:
+                price = 0
+            return price
+
+    def thesource(self, sn=None):
+        if sn is None:
+            self.get_content(self.url)
+            soup = self.soup
+
+            name = soup.find('h1', {'class': 'pdp-name'}).find('span')
+            price = soup.find('div', {'class': 'pdp-sale-price'})
+            asin = soup.find('span', {'class': 'identifier'})
+            date = today.strftime("%d/%m/%Y")
+            all = {}
+
+            if asin is not None:
+                all['id'] = asin.text
+            else:
+                all['id'] = '-1'
+
+            if name is not None:
+                all['name'] = name.text.strip()
+            else:
+                all['name'] = None
+
+            if price is not None:
+                all['price'] = {date: price.text.strip().replace(
+                    u'\xa0', u'')}
+            else:
+                all['price'] = {date: 'CDN$0'}
+
+            return all
+
+        else:
+            url = 'https://www.thesource.ca/en-ca/search?text=' + sn
+            session = HTMLSession()
+            r = session.get(url)
+            r.html.render()
+            try:
+                price = r.html.find('div.sale-price',
+                                    first=True).text
+            except Exception:
+                print('The Source: Price not found.')
+                price = 0
+            return price
+
+    def canadacomputers(self, sn=None):
+        if sn is None:
+            self.get_content(self.get_content)
+            soup = self.soup
+
+            name = soup.find(
+                'h1', {'class': 'h3 product-title mb-2'}).find('strong')
+            price = soup.find('span', {'class': 'h2-big'}).find('strong')
+            asin = soup.find('p', {'class': 'm-0 text-small'})
+            date = today.strftime("%d/%m/%Y")
+            all = {}
+
+            if asin is not None:
+                all['id'] = asin.text.replace('Item Code:  ', '')
+            else:
+                all['id'] = '-1'
+
+            if name is not None:
+                all['name'] = name.text.strip()
+            else:
+                all['name'] = None
+
+            if price is not None:
+                all['price'] = {date: price.text.strip().replace(
+                    u'\xa0', u'')}
+            else:
+                all['price'] = {date: 'CDN$0'}
+
+            return all
+
+        else:
+            url = 'https://www.canadacomputers.com/search/results_details.php?language=en&keywords=' + sn
+            self.get_content(url)
+            soup = self.soup
+
+            try:
+                price = soup.find(
+                    id='product-list').find('div', {'class': 'row mx-0'}).find('strong').text
+                print(price)
+            except Exception:
+                price = 0
+                print(price)
 
             return price
 
-    def newegg(self):
-        # requests-html needed to parse JavaScript (price)
-        session = HTMLSession()
-        r = session.get(self.url)
-        r.html.render()
+    def memoryexpress(self, sn=None):
+        if sn is None:
+            self.get_content(self.url)
+            soup = self.soup
 
-        soup = self.soup
+            name = soup.find(
+                'header', {'class': 'c-capr-header'}).find('h1')
+            price = soup.find(
+                'div', {'class': 'GrandTotal c-capr-pricing__grand-total'}).find('div')
+            asin = soup.find(
+                'article', {'class': 'l-capr-page'})['data-product-id']
+            date = today.strftime("%d/%m/%Y")
+            all = {}
 
-        name = soup.find('span', {'style': 'display: inline;'})
-        price = r.html.find('li.price-current', first=True).text.strip()
-        # rating = soup.find('span', attrs={'class': 'print'})
-        asin = soup.find('dt', string='Model').next_sibling
-        date = today.strftime("%d/%m/%Y")
-        all = {}
+            if asin is not None:
+                all['id'] = asin
+            else:
+                all['id'] = '-1'
 
-        if asin is not None:
-            all['id'] = asin.text
+            if name is not None:
+                all['name'] = name.text.strip()
+            else:
+                all['name'] = None
+
+            if price is not None:
+                all['price'] = {date: price.text.strip().replace(
+                    'Only', '')}
+            else:
+                all['price'] = {date: 'CDN$0'}
+
+            return all
+
         else:
-            all['id'] = '-1'
+            url = 'https://www.memoryexpress.com/Search/Products?Search=' + sn
+            self.get_content(url)
+            soup = self.soup
 
-        if name is not None:
-            all['name'] = name.text.strip()
+            try:
+                price = soup.find(
+                    'div', {'class': 'c-shca-icon-item__summary-list'}).find('span').text.strip()
+                print(price)
+            except Exception:
+                price = 0
+                print(price)
+
+            return price
+
+    def mikescomputershop(self, sn=None):
+        if sn is None:
+            self.get_content(self.url)
+            soup = self.soup
+
+            name = soup.find('div', {'class': 'gd-1 Title'})
+            price = soup.find('div', {'class': 'Price Special'})
+            if price is None:
+                price = soup.find('div', {'class': 'retail'})
+
+            asin = soup.find(
+                'dt', string='Product Model').next_element.next_element.next_element
+            date = today.strftime("%d/%m/%Y")
+            all = {}
+
+            if asin is not None:
+                all['id'] = asin.text.strip()
+            else:
+                all['id'] = '-1'
+
+            if name is not None:
+                all['name'] = name.text.strip()
+            else:
+                all['name'] = None
+
+            if price is not None:
+                all['price'] = {date: price.text}
+            else:
+                all['price'] = {date: 'CDN$0'}
+
+            return all
+
         else:
-            all['name'] = None
+            url = 'https://www.mikescomputershop.com/catalog/?q=' + sn
+            driver = webdriver.Firefox(options=options)
+            driver.get(url)
+            try:
+                price = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, "//div/div/ul/li/div"))).text
+            finally:
+                driver.quit()
+            return price
 
-        if price is not None:
-            all['price'] = {date: price.replace(
-                u'\xa0', u'').replace(' –', '')}
-        else:
-            all['price'] = {date: 'CDN$0'}
-
-        return all
-
-    def thesource(self):
-        soup = self.soup
-
-        name = soup.find('h1', {'class': 'pdp-name'}).find('span')
-        price = soup.find('div', {'class': 'pdp-sale-price'})
-        asin = soup.find('span', {'class': 'identifier'})
-        date = today.strftime("%d/%m/%Y")
-        all = {}
-
-        if asin is not None:
-            all['id'] = asin.text
-        else:
-            all['id'] = '-1'
-
-        if name is not None:
-            all['name'] = name.text.strip()
-        else:
-            all['name'] = None
-
-        if price is not None:
-            all['price'] = {date: price.text.strip().replace(
-                u'\xa0', u'')}
-        else:
-            all['price'] = {date: 'CDN$0'}
-
-        return all
-
-    def canadacomputers(self):
-        soup = self.soup
-
-        name = soup.find(
-            'h1', {'class': 'h3 product-title mb-2'}).find('strong')
-        price = soup.find('span', {'class': 'h2-big'}).find('strong')
-        asin = soup.find('p', {'class': 'm-0 text-small'})
-        date = today.strftime("%d/%m/%Y")
-        all = {}
-
-        if asin is not None:
-            all['id'] = asin.text.replace('Item Code:  ', '')
-        else:
-            all['id'] = '-1'
-
-        if name is not None:
-            all['name'] = name.text.strip()
-        else:
-            all['name'] = None
-
-        if price is not None:
-            all['price'] = {date: price.text.strip().replace(
-                u'\xa0', u'')}
-        else:
-            all['price'] = {date: 'CDN$0'}
-
-        return all
-
-    def memoryexpress(self):
-        soup = self.soup
-
-        name = soup.find(
-            'header', {'class': 'c-capr-header'}).find('h1')
-        price = soup.find(
-            'div', {'class': 'GrandTotal c-capr-pricing__grand-total'}).find('div')
-        asin = soup.find(
-            'article', {'class': 'l-capr-page'})['data-product-id']
-        date = today.strftime("%d/%m/%Y")
-        all = {}
-
-        if asin is not None:
-            all['id'] = asin
-        else:
-            all['id'] = '-1'
-
-        if name is not None:
-            all['name'] = name.text.strip()
-        else:
-            all['name'] = None
-
-        if price is not None:
-            all['price'] = {date: price.text.strip().replace(
-                'Only', '')}
-        else:
-            all['price'] = {date: 'CDN$0'}
-
-        return all
-
-    def mikescomputershop(self):
-        soup = self.soup
-
-        name = soup.find('div', {'class': 'gd-1 Title'})
-        price = soup.find('div', {'class': 'Price Special'})
-        if price is None:
-            price = soup.find('div', {'class': 'retail'})
-
-        asin = soup.find(
-            'dt', string='Product Model').next_element.next_element.next_element
-        date = today.strftime("%d/%m/%Y")
-        all = {}
-
-        if asin is not None:
-            all['id'] = asin.text.strip()
-        else:
-            all['id'] = '-1'
-
-        if name is not None:
-            all['name'] = name.text.strip()
-        else:
-            all['name'] = None
-
-        if price is not None:
-            all['price'] = {date: price.text}
-        else:
-            all['price'] = {date: 'CDN$0'}
-
-        return all
-
-    # def check_all(self, start):
-    #     if start ==
+        # return price
+        # def check_all(self, start):
+        #     if start ==
 
 
 if __name__ == '__main__':
     scraper = Scrape()
-    scraper.bestbuy('dell')
+    scraper.mikescomputershop('ryzen')
